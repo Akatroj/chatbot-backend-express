46713bca01203a5a5aa68844fc28ff701529799b
Akatroj <yjkjgdjfhjrdhgjh@gmail.com>
2021-11-25 17:08:28 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { google } = require('googleapis');
+const { WebhookClient } = require('dialogflow-fulfillment');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Dziala'));
+
+app.post('/dialogflow', (req, res) => {
+  const agent = new WebhookClient({ request: req, response: res });
+  const intentMap = new Map();
+  const intentName = 'Wyliczenieceny.Wyliczenieceny-custom.Iloduychpaneli-custom.Ilorednichpaneli-next.Ilomaychpaneli-yes'; // i'm sorry...
+  intentMap.set(intentName, intentHandler);
+  agent.handleRequest(intentMap);
+});
+
+function intentHandler(agent) {
+  agent.add("dzialam!");
+}
