afcd045ae616462235de7fdb7b3a7ada52af0bb4
Akatroj <yjkjgdjfhjrdhgjh@gmail.com>
2021-11-26 15:49:32 +0100

    Added cost calculation

--- index.js
+++ index.js
@@ -13,14 +13,19 @@ app.get('/', (req, res) => res.send('Dziala'));
 app.post('/dialogflow', (req, res) => {
   const agent = new WebhookClient({ request: req, response: res });
   const intentMap = new Map();
-  const intentName = 'Rezultat';
-  intentMap.set(intentName, intentHandler);
+  intentMap.set('Oblicz-koszt', calculateCost);
   agent.handleRequest(intentMap);
   console.log(agent.intent);
 });
 
-function intentHandler(agent) {
-  agent.add("dzialam!");
+function calculateCost(agent) {
+  console.log(agent.parameters.panele);
+  const [duze, srednie, male] = [...agent.parameters.panele, 0, 0, 0]; // jak dialogflow zczyta mniej niz 3 parametry to je tutaj wyzerujemy
+
+  let cena = 8000 * male + 18000 * srednie + 36000 * duze;
+  cena = (cena > 0) ? cena + 100 : 0;
+  agent.clearOutgoingContexts();
+  agent.add(`Instalacja ${duze} dużych paneli, ${srednie} średnich paneli oraz ${male} małych paneli kosztowałaby około ${cena} złotych.`);
 }
 
 app.listen(port, () => {
